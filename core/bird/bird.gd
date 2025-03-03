c70f4dec3e73a683f1484f3904bef3d42d1f4e6a
ky2312 <ky2312@outlook.com>
2025-03-03 15:38:04 +0800

    添加资源

--- core/bird/bird.gd
+++ core/bird/bird.gd
@@ -7,17 +7,31 @@ signal exited
 const GRAVITY = 980
 # 升力速度
 const LIFT = 250
+# 是否持续飞行
+var is_keep_fly = false
 
 func init(pos: Vector2):
 	position = pos
 
 func _physics_process(delta: float) -> void:
+	if is_keep_fly:
+		velocity = Vector2.UP * LIFT
+		
 	velocity += Vector2.DOWN * GRAVITY * delta
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("fly"):
-		velocity = Vector2.UP * LIFT
+		if not is_keep_fly:
+			$Image.play("up")
+		else:
+			$Image.play("flap")
+			
+		is_keep_fly = true
+	else:
+		is_keep_fly = false
+		if $Image.animation != "down":
+			$Image.play("down")
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
--- core/wood/wood_manager.gd
+++ core/wood/wood_manager.gd
@@ -8,7 +8,8 @@ signal touch
 func create():
 	var w = wood.instantiate()
 	var w_pos = $WoodMarker.position
-	w_pos.y -= randf_range(0.0, get_viewport_rect().size.y / 2)
+	var max_h_offset = get_viewport_rect().size.y * 0.2
+	w_pos.y += randf_range(max_h_offset * -1, max_h_offset)
 	w.init(w_pos)
 	w.connect("touch", func():
 		touch.emit()
